6c2c8eea898eff306642eccfa32699207a3cc393
Tremelune <tremelune@ensaster.com>
2011-07-20 16:06:14 -0400

    Initial commit balls

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '66be6a766c4dec1cd0db97ab35053bfe1be11fb96e411a5043c182268e7ce8fb4ac65e0b52917f3a8dd4a9b77c782ac44a792720f3eb6d121d2ecaf8d5ab811c'
